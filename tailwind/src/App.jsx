33e04ae44c74059ee57fa37acdc38e6cc0d4cdfa
sahar sabeel <sapienktk@gmail.com>
2024-05-12 15:59:24 +0500

    react/tailwind-intro

---
+++ tailwind/src/App.jsx
@@ -0,0 +1,20 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './component/Card'
+function App() {
+  //  let myObj = {
+  //   course:' react',
+  //   price : 5000
+  //  }
+  return (
+    <>
+     <h1 className='bg-green-400 mb-10'> hello to Tailwind </h1>
+     <Card userName ="sara"/>
+     <Card userName= "sara"/>
+    </>
+  )
+}
+
+export default App
